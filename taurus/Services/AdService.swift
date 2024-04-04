30b9225156170938b05848190e813a9b5e07449a
Felipe Passos <berkspar@gmail.com>
2024-04-03 23:16:26 -0300

    fix

--- taurus/Services/AdService.swift
+++ taurus/Services/AdService.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class AdService: ObservableObject {
-    // Production
-    static let gameView = "ca-app-pub-2005622694052245/3042641245"
-    static let rewardedId = "ca-app-pub-2005622694052245/1653940901"
-    static let intersticalId = "ca-app-pub-3940256099942544/4411468910"
-    
-    // Test
-//    static let gameView = "ca-app-pub-2005622694052245/3042641245"
-//    static let rewardedId = "ca-app-pub-2005622694052245/1653940901"
-//    static let intersticalId = "ca-app-pub-3940256099942544/4411468910"
+    #if DEBUG
+        static let gameView = "ca-app-pub-3940256099942544/9214589741"
+        static let rewardedId = "ca-app-pub-3940256099942544/5224354917"
+        static let intersticalId = "ca-app-pub-3940256099942544/1033173712"
+    #else
+        static let gameView = "ca-app-pub-2005622694052245/3042641245"
+        static let rewardedId = "ca-app-pub-2005622694052245/1653940901"
+        static let intersticalId = "ca-app-pub-2005622694052245/4628223062"
+    #endif
 }
